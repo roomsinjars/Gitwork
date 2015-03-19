a2dd07b4fc4bdc66e8d98dd86e6e878b1b17751f
Ben Levine <Benjamin.R.Levine@gmail.com>
2015-03-19 15:22:53 -0400

    finished homepage directives

--- window/app.js
+++ window/app.js
@@ -5,5 +5,24 @@ var app = angular.module('GitWork', []);
 app.controller('MainController', function ($scope) {
 
 
+});
+
+app.directive('home', function () {
+
+    return {
+        restrict: 'E',
+        scope: {},
+        templateUrl: 'window/directives/home/home.html'
+    };
+
+});
+
+app.directive('navbar', function () {
+
+    return {
+        restrict: 'E',
+        scope: {},
+        templateUrl: 'window/directives/common/navbar/navbar.html'
+    };
 
 });
