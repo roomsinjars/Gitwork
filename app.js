b7b7169c42c104697944ebbff2a67d4a4e68a8e7
blakeprobinson <bprobinson@zoho.com>
2015-03-21 14:58:05 -0400

    testing addFiles button

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 var app = angular.module('GitWork', ['ui.router']);
 
 
+
 app.config(function($stateProvider, $urlRouterProvider){
 
     $stateProvider
@@ -58,6 +59,9 @@ app.controller('MainController', function ($scope, repoFactory, $rootScope) {
       repoFactory.createBranch(branchName, $rootScope.repoName )
       $scope.branchName = ''
     }
+    $scope.addFilesToIndex = function () {
+      repoFactory.addFiles($rootScope.repoName)
+    }
 
 
 });
@@ -73,6 +77,170 @@ app.factory('repoFactory', function ($rootScope){
 
     
     return {
+
+        pull: function () {
+
+          var repoDir = "../../test";
+
+          var repository;
+
+          // Open a repository that needs to be fetched and fast-forwarded
+          NodeGit.Repository.open(path.resolve(__dirname, repoDir))
+            .then(function(repo) {
+              repository = repo;
+
+              return repository.fetchAll({
+                credentials: function(url, userName) {
+                  return NodeGit.Cred.sshKeyFromAgent(userName);
+                },
+                certificateCheck: function() {
+                  return 1;
+                }
+              });
+            })
+            // Now that we're finished fetching, go ahead and merge our local branch
+            // with the new one
+            .then(function() {
+              return repository.mergeBranches("master", "origin/master");
+            })
+            .done(function() {
+              console.log("Done!");
+            });
+        },
+
+        addFiles: function (repoName) {
+            var Promise = require("nodegit-promise");
+
+            NodeGit.Repository.open(path.resolve(__dirname + '/' + repoName))
+              .then(function(repo) {
+                return repo.openIndex()
+                .then(function(index) {
+                  var fileContent = {
+                    newFile1: "this has some content",
+                    newFile2: "and this will have more content"
+                  };
+                  var fileNames = Object.keys(fileContent);
+
+                  return Promise.all(fileNames.map(function(fileName) {
+                    fse.writeFile(
+                      path.join(repo.workdir(), fileName), fileContent[fileName]);
+                  }))
+
+
+
+                    // This will add all files to the index
+                  .then(function() {
+                    return index.addAll();
+
+                  })
+                  .then(function() {
+                    var newFiles = index.entries().filter(function(entry) {
+                      return ~fileNames.indexOf(entry.path);
+                    });
+
+                    console.log(
+                      "\n-------------------\n" +
+                      "Added files: " +
+                      "\n-------------------\n");
+                    newFiles.forEach(function(entry) {
+                      console.log(entry.path);
+                    });
+                  })
+                  // .then(function() {
+                  //   // This will remove the files from the index
+                  //   return index.removeAll("newFile*");
+                  // })
+                  // .then(function() {
+                  //   var newFiles = index.entries().filter(function(entry) {
+                  //     return ~fileNames.indexOf(entry.path);
+                  //   });
+
+                  //   console.log("New files in index: " + newFiles.length);
+                  // })
+
+
+
+                  // We can also provide a pattern to add files to the index
+                  // .then(function() {
+                  //   return index.addAll("newFile*");
+                  // })
+                  // .then(function() {
+                  //   var newFiles = index.entries().filter(function(entry) {
+                  //     return ~fileNames.indexOf(entry.path);
+                  //   });
+
+                  //   console.log(
+                  //     "\n-------------------\n" +
+                  //     "Added files with pattern: " +
+                  //     "\n-------------------\n");
+                  //   newFiles.forEach(function(entry) {
+                  //     console.log(entry.path);
+                  //   });
+                  // })
+                  // .then(function() {
+                  //   // We're also using the pattern in the remove
+                  //   return index.removeAll("newFile*");
+                  // })
+                  // .then(function() {
+                  //   var newFiles = index.entries().filter(function(entry) {
+                  //     return ~fileNames.indexOf(entry.path);
+                  //   });
+
+                  //   console.log("New files in index: " + newFiles.length);
+                  // })
+
+
+
+                  // Callbacks can be used for a finer degree of control over what
+                  // we add to the index
+                  // .then(function() {
+                  //   return index.addAll(
+                  //     "newFile*",
+                  //     NodeGit.Index.ADD_OPTION.ADD_CHECK_PATHSPEC,
+                  //     function(path, matchedPattern) {
+                  //       if (path == "newFile1") {
+                  //         return 0; // add the file
+                  //       }
+
+                  //       return 1; // skip the file
+                  //     });
+                  // })
+                  // .then(function() {
+                  //   var newFiles = index.entries().filter(function(entry) {
+                  //     return ~fileNames.indexOf(entry.path);
+                  //   });
+
+                  //   console.log(
+                  //     "\n-------------------\n" +
+                  //     "Added files with callback: " +
+                  //     "\n-------------------\n");
+                  //   newFiles.forEach(function(entry) {
+                  //     console.log(entry.path);
+                  //   });
+                  // })
+                  // .then(function() {
+                  //   // Lets use a callback in the remove as well
+                  //   return index.removeAll(null, function(path) {
+                  //     if (~path.indexOf("newFile")) {
+                  //       return 0; // remove the file
+                  //     }
+
+                  //     return 1; // don't remove the file
+                  //   });
+                  // })
+                  // .then(function() {
+                  //   var newFiles = index.entries().filter(function(entry) {
+                  //     return ~fileNames.indexOf(entry.path);
+                  //   });
+
+                  //   console.log("Total: " + index.entries().length);
+                  //   console.log("New files in index: " + newFiles.length);
+                  // });
+                });
+              }).done(function() {
+                console.log("All done!");
+              });
+        },
 
         cloneRepo: function(url){
             var cloneURL = url;
