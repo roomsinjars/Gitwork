eeb5c33b27924f12e0c67ac12340addca45c10d1
Lynda Montgomery <lynda.montgomery@me.com>
2015-03-24 16:11:54 -0400

    install gw globally

--- public/app.js
+++ public/app.js
@@ -108,10 +108,16 @@ app.controller('HomeController', function ($scope, $state) {
     };
     console.log("HomeController", install.value);
     if (install.value==="false") {
-        
-        console.log("got here");
+        //npm link on the current directory
+        var exec = require('child_process').exec;
+        exec('npm link', function(error,stdout){
+            console.log('installed', stdout);
+            fs.writeFile('install.json', '{"value": "true"}', function(err){
+                if (err) throw err;
+                console.log('done');
+            })
+        })
     }
-
     fs.readdir(__dirname, function(err,data){
         if (err) throw err;
         for (var i=0; i<data.length; i++){
