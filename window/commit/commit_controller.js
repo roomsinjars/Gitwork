afecd83881815018249be4a28c0b29f51ee493a1
Lynda <lynda.montgomery@me.com>
2015-03-31 16:08:06 -0400

    moved functionality to factory

--- window/commit/commit_controller.js
+++ window/commit/commit_controller.js
@@ -1,4 +1,4 @@
-app.controller('CommitCtrl', function ($scope, $state, $rootScope, Status, repoFactory, commitFactory) {
+app.controller('CommitCtrl', function ($scope, $state, $rootScope, repoFactory, commitFactory) {
 	
 	$scope.commit = function (commitMsg) {
 		repoFactory.commit($rootScope.repo, commitMsg)
@@ -14,20 +14,33 @@ app.controller('CommitCtrl', function ($scope, $state, $rootScope, Status, repoF
 		commitFactory.removeFiles(file);
 		$scope.getStatus();
 	}
-
-	$scope.getStatus = function() {
-		Status.get().then(function(data) {
-			$scope.filesUnstaged = [];
-			$scope.filesStaged = [];
-			console.log('data', data);
-			for (var i=0; i< data.length; i++){
-				console.log(data[i].tracked);
-					if (data[i].staged == false) {
-						$scope.filesUnstaged.push(data[i]);
-					} else {
-						$scope.filesStaged.push(data[i]);
-					}
-			}
-		})
+	
+	$scope.getStatus = function () {
+		commitFactory.unstaged()
+			.then(function(data) {
+				console.log('unstaged :', $scope.filesUnstaged);	
+				return $scope.filesUnstaged = data;
+			})
+		commitFactory.staged()
+			.then(function(data) {
+				console.log('staged ', $scope.filesStaged);	
+				return $scope.filesStaged = data;
+			})
 	}
+
 });
+	// $scope.getStatus = function() {
+	// 	Status.get().then(function(data) {
+	// 		$scope.filesUnstaged = [];
+	// 		$scope.filesStaged = [];
+	// 		console.log('data', data);
+	// 		for (var i=0; i< data.length; i++){
+	// 			console.log(data[i].tracked);
+	// 				if (data[i].staged == false) {
+	// 					$scope.filesUnstaged.push(data[i]);
+	// 				} else {
+	// 					$scope.filesStaged.push(data[i]);
+	// 				}
+	// 		}
+	// 	})
+	// }
