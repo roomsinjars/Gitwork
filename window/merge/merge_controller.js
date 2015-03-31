0c99aab93c5b7efc370e9f9066d2fd2c16b18edb
blakeprobinson <bprobinson@zoho.com>
2015-03-31 13:51:15 -0400

    merge function is merging with issues

--- window/merge/merge_controller.js
+++ window/merge/merge_controller.js
@@ -1,8 +1,40 @@
 app.controller('MergeCtrl', function ($scope, repoFactory, $rootScope, mergeFactory) {
-	console.log(mergeFactory)
     $scope.merge = function () {
-        mergeFactory.merge()
-        $scope.mergeConflictError = repoFactory.mergeConflictError
+        // mergeFactory.mergeSpawn().then(function (data) {
+        //     console.log('this is the postmerge data', data)
+        // })
+        // .catch(function (err) {
+        //     console.log('this is the error', err)
+        // })
+    	console.log($rootScope.repo.path)
+        mergeFactory.getRepo($rootScope.repo.path)
+        	.then(mergeFactory.getBranchCommit)
+        	.then(mergeFactory.getMasterCommit)
+        	.then(mergeFactory.mergeCommits)
+        	.then(mergeFactory.addToIndex)
+        	.then(mergeFactory.postMergeCommit)
+        	// .then(function (repo) {
+        	// 	console.log('got here', repo)
+        	// })
+        	.catch(function (err) {
+                console.log(err)
+                if (err.message === 'This merge has conflicts') {
+                    mergeFactory.getConflicts()
+                        .then(function (conflictFiles) {
+                            $scope.conflictFiles = conflictFiles
+                            $scope.mergeErr = err.message
+                    })
+                } else {
+                    $scope.mergeErr = err.message
+                }
+        		
+        	})
+        	.then(function (repo) {
+        	 	$scope.mergeSuccess = true;
+        	})
+        	
+
+        
     }
 
     $scope.findConflicts = function () {
--- window/merge/merge_factory.js
+++ window/merge/merge_factory.js
@@ -1,87 +1,169 @@
-app.factory('mergeFactory', function ($rootScope, $q, branchFactory, fsFactory) {
+app.factory('mergeFactory', function (
+	$rootScope, $q, branchFactory, fsFactory, repoFactory) {
 	var self = this;
-	console.log('this is this', this)
 	return {
-		
-		merge: function () {
-			var closure = {};
-	    var ourSignature = NodeGit.Signature.now($rootScope.username,
-	      $rootScope.useremail);
-	    NodeGit.Repository.open($rootScope.repo.path)
-	        .then(function(repository) {
-	            if (branchFactory.currentBranch !== 'master') {
-	                closure.repo = repository
-	                return closure.repo.getBranchCommit(branchFactory.currentBranch)
-	            } else {
-	                throw new Error('You should not merge master into master')
-	            }
-	            })                    
-	        .then(function (commitBranch) {
-	        	console.log('this is the commit Branch', commitBranch.repo)
-	        	closure.commitBranch = commitBranch
-	            return closure.repo.getBranchCommit('master')	            	
-	           	})
-	        .then(function (commitMaster) {
-	        	closure.commitMaster = commitMaster
-	            return NodeGit.Merge.commits(closure.repo, closure.commitBranch, closure.commitMaster)
-	        })
-	        .then(function (index) {
-	            if (!index.hasConflicts()) {
-	                index.write();
-	                console.log('this is the index', index)
-	                return index.writeTreeTo(closure.repo);
-	            } else {
-	                //on branch...findInFiles('<<<<<<<<master')
-	                //$Q is the promise library for angular
-	                throw new Error('This merge has conflicts')
-	                //get user to fix merge conflicts before writing
-	                //to tree...
-	            }   
-	            })
-	        .then(function (oid) {
-	            console.log('this is the oid', oid)
-	            return closure.repo.createCommit('HEAD', ourSignature,
-	                ourSignature, "we merged their commit", oid, [closure.commitBranch, closure.commitMaster]);
-	        })
-	        .catch(function(error) {
-	            $rootScope.mergeConflictError = error;
-	            console.log($rootScope.mergeConflictError)
-	            this.mergeConflictError(error)
-	        })
-	        .done(function (commitId) {
-	            if (commitId) {
-	                console.log("New Commit: ", commitId);
-	            }
-	            
-	        });
+
+		getRepo: function (path) {
+			return $q(function (resolve, reject) {
+				var repo = {}
+				if (!path) {
+					var pathError = new Error('repo path is not defined')
+					reject(pathError)
+				} else {
+					NodeGit.Repository.open(path)
+				        .then(function(repository) {
+				        	repo.repo = repository
+				        	var signature = NodeGit.Signature.default(repository)
+				        	//signature = signature.toString()
+				        	repo.signature = signature
+				        	console.log('getRepo repo Object', repo)
+							resolve(repo)
+					})
+				}
+			})
 		},
-		// findConflicts: function () {
-		// 	var closure = {}
-		// 	fsFactory.findFilesInDir($rootScope.repo.path +'/test', ['.git'])
-		// 		.then(function (files) {
-		// 			self.getConflicts()
-		// 			closure.files = files
-		// 			console.log('these are the files', closure.files)
-		// 			return fsFactory.arrayMap(files, fsFactory.readFile);
-		// 		})
-		// 		.then(function (arrayOfContents) {
-		// 			closure.contents = arrayOfContents
-		// 			return fsFactory.arrayMap(arrayOfContents, fsFactory.findInFile)
-		// 		})
-		// 		.then(function (arrayOfBooleans) {
-		// 			closure.booleans = arrayOfBooleans
-		// 			closure.conflictFiles = []
-		// 			var booleanIndex = 0;
-		// 			console.log('this is the array of Booleans', closure.booleans)
-		// 			for (var i = 0, len = arrayOfBooleans.length; i < len; i++) {
-		// 				if (arrayOfBooleans[i]) {
-		// 					closure.conflictFiles.push(closure.contents[i])
-		// 				}
-		// 			}
-		// 			console.log(closure.conflictFiles)
-		// 		})
-		// },
+
+		getBranchCommit: function (repo) {
+			return $q(function (resolve, reject) {
+				repo.currentBranch = branchFactory.currentBranch
+				console.log('this is repo.currentBranch', repo.currentBranch)
+				if (!repo.currentBranch || !repo) {
+					var branchErr = new Error('branch is not defined')
+					reject(branchErr)
+				} else {
+					repo.repo.getBranchCommit(repo.currentBranch)
+						.then(function (branchCommit) {
+							repo.branchCommit = branchCommit;
+							console.log('repo after getbranchCommit', repo)
+							resolve(repo)
+							
+						})
+				}
+			})
+		},
+
+		getMasterCommit: function (repo) {
+			return $q(function (resolve, reject) {
+				if (!repo) {
+					var repoErr = new Error('repo is not defined')
+					reject(branchErr)
+				} else {
+					repo.repo.getBranchCommit('master')
+						.then(function (masterCommit) {
+							repo.masterCommit = masterCommit;
+							console.log('repo after getMasterCommit', repo)
+							resolve(repo)
+							
+						})
+				}
+			})
+		}, 
+		
+		mergeCommits: function (repo) {
+			return $q(function (resolve, reject) {
+				var mergeOptions = new NodeGit.MergeOptions()
+				mergeOptions.fileFavor = 2
+				NodeGit.Merge.commits(repo.repo, repo.branchCommit, repo.masterCommit, mergeOptions)
+					.then(function (index) {
+						if (!index.hasConflicts()) {
+			                index.write();
+			                console.log('this is the index', index)
+			                repo.index = index;
+			                console.log('repo at mergeCommits', repo)
+			                resolve(repo);
+			            } else {
+			                var conflictErr = new Error('This merge has conflicts')
+			                reject(conflictErr)
+			            }  
+			        })
+				})				
+		},
+
+		addToIndex: function (repo) {
+			return $q(function (resolve, reject) {
+				repo.index.writeTreeTo(repo.repo)
+					.then(function (oid) {
+					    console.log('this is the oid', oid)
+					    repo.oid = oid
+					    console.log('repo at addToIndex', repo)
+					    resolve(repo)	    
+				})
+			})
+		},
+
+		postMergeCommit: function (repo) {
+			return new $q(function (resolve, reject) {
+				// var commitMsg = 'merged master into ' + repo.currentBranch
+				// var options = {
+				// 	all: true,
+				// 	amend: false,
+				// 	author: repo.signature
+				// }
+				// var commitMsg = 'merged master into ' + repo.currentBranch
+				// $rootScope.repo.commit(commitMsg, options, function (err) {
+				// 	console.log(err)
+				// 	if (err) {
+				// 		reject(err)
+				// 	} else {
+				// 		repo.postMergeCommit = true;
+				// 		resolve(repo)
+				// 		console.log('postMergeCommit', repo)
+				// 	}
+				// })
+
+
+				repo.repo.createCommit('HEAD', 
+					repo.signature,
+					repo.signature, 
+					"merged master into" + repo.currentBranch, 
+					repo.oid, 
+					[repo.branchCommit, repo.masterCommit])
+				.then(function (commitId) {
+					repo.postMergeCommitId = commitId
+					console.log('repo at postMergeCommit', repo)
+					resolve(repo)
+				})
+				.catch(function (err) {
+					console.log('this is the error', err)
+				})
+			})
+		},
+
+
+		errorHandler: function (reason) {
+			return $q(function (resolve, reject) {
+				if (reason.message === 'This merge has conflicts') {
+        			reject(reason)
+        		} else {
+        			resolve(reason)
+        		}
+			})
+		},
+
+		mergeSpawn: function () {
+			return $q(function (resolve, reject) {
+				var git = spawn('git', ['merge', 'master']); 
+				git.stdout.on('data', function (data) {
+					console.log('this is the raw', data)
+				  var strData = ''+ data;
+				  var arrStrData = [];
+				  console.log('this is the raw data', strData);
+				  var len = arrStrData.length;
+				  arrStrData = strData.split("\n").slice(0, len-1)
+				  console.log(arrStrData)
+				  if (arrStrData.length >=1) {
+				  		resolve(arrStrData)
+				  } else {
+				  	  var error = 'No files are in conflict';
+				  	  reject(error)
+				  }			  
+				});
+			})
+		},
+
 		getConflicts: function () {
+
+			//this is happening at the wrong working directory
 			return $q(function (resolve, reject) {
 				var git = spawn('git', ['diff-files', '--name-only']);
 				git.stdout.on('data', function (data) {
@@ -96,8 +178,7 @@ app.factory('mergeFactory', function ($rootScope, $q, branchFactory, fsFactory) 
 				  } else {
 				  	  var error = 'No files are in conflict';
 				  	  reject(error)
-				  }
-				  
+				  }			  
 				});
 			})
 		}
