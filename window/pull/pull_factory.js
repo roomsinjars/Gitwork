368d2f28848d1f6d0a7906f5afc214b0005e96f3
Lynda <lynda.montgomery@me.com>
2015-03-31 17:17:05 -0400

    only can merge all files have been commited

--- window/pull/pull_factory.js
+++ window/pull/pull_factory.js
@@ -1,16 +1,19 @@
-app.factory('pullFactory', function ($rootScope, branchFactory){
+app.factory('pullFactory', function ($rootScope, $state, commitFactory){
 
     return {
         pullRepo: function(){
-        	console.log(branchFactory.currentBranch.length);
-        	if (branchFactory.currentBranch.length < 1){
-	          $rootScope.repo.remote_fetch("", function(err){
-	              if (err) throw err;
-	              console.log("fetched repo")
-	          })
-        	} else {
-        		$state.go('commit');
-        	}
+        	commitFactory.unstaged().then(function(data){
+        		console.log('pull', data);
+	        	if (data.length < 1){
+		          $rootScope.repo.remote_fetch("", function(err){
+		              if (err) throw err;
+		              console.log("fetched repo")
+		          })
+	        	} else {
+	        		$state.go('commit');
+	        	}
+        		
+        	})
         }
     }
 });
