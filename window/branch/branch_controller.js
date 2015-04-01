3fb8e12c878f7c8d1aba7a6ec728269b2c0ac9ff
Lynda <lynda.montgomery@me.com>
2015-04-01 11:23:33 -0400

    inputs must be at least 4 characters long

--- window/branch/branch_controller.js
+++ window/branch/branch_controller.js
@@ -3,17 +3,20 @@ app.controller('BranchCtrl', function ($scope, $state, $rootScope, branches, bra
   $scope.branches = branches;
 
   $scope.switch = function (branchName) {
+    console.log('switch', branchName);
   	branchFactory.switchBranch(branchName);
   	branchFactory.currentBranch = branchName;
   }
 
   $scope.newBranch = function(branchName) {
+    console.log('initial', $scope.branchName);
     branchFactory.switchBranch('master')
       .then(function (data) {
+        console.log('data', data, 'branchName', branchName)
         return branchFactory.createNewBranch(branchName)
       }).then(function (data) {
         branchFactory.currentBranch = branchName;
-        console.log(branchFactory.currentBranch)
+        console.log('bf', branchFactory.currentBranch, 'name', branchName)
         return branchFactory.switchBranch(branchName)
       }).then(function (data) {
         $state.go('work');
