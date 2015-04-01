c5b5b0fa132d38a2b09cb96f0b064acae3de57aa
blakeprobinson <bprobinson@zoho.com>
2015-04-01 12:52:50 -0400

    added fetch to merge

--- window/merge/merge_controller.js
+++ window/merge/merge_controller.js
@@ -1,61 +1,32 @@
-app.controller('MergeCtrl', function ($scope, repoFactory, $rootScope, mergeFactory, $state) {
-    $scope.merge = function () {
-        // mergeFactory.mergeSpawn().then(function (data) {
-        //     console.log('this is the postmerge data', data)
-        // })
-        // .catch(function (err) {
-        //     console.log('this is the error', err)
-        // })
-        mergeFactory.mergeExec().then(function (arrStrData) {
+app.controller('MergeCtrl', function 
+    ($scope, repoFactory, $rootScope, mergeFactory, $state, branchFactory, pullFactory) {
+    $scope.merge = function () {        
+
+        branchFactory.switchBranch('master').then(function (data) {
+            return pullFactory.pullRepo()
+        })
+        .then(function (data) {
+            return branchFactory.switchBranch(branchFactory.currentBranch)
+        })
+        .then(function (data) {
+            return mergeFactory.mergeExec()
+        })
+        .then(function (arrStrData) {
             if (arrStrData[1] && arrStrData[1].slice(0,8)==="CONFLICT") {
                 $scope.mergeConflicts = arrStrData
             } else if (arrStrData[0].slice(0,7)==="Already") {
                  $scope.alreadyMerged = arrStrData;
             } else {
                 $state.go('merge-success')
-            }
-            
+            }    
         })
         .catch(function (errArr) {
             if (errArr[1] === "error: 'merge' is not possible because you have unmerged files.") {
                 console.log('this is the errArr in the dotCatch', errArr)
                 $scope.mergeErr = errArr
             }
-
-            
-
         })
-    	// console.log($rootScope.repo.path)
-     //    mergeFactory.getRepo($rootScope.repo.path)
-     //    	.then(mergeFactory.getBranchCommit)
-     //    	.then(mergeFactory.getMasterCommit)
-     //    	.then(mergeFactory.mergeCommits)
-     //    	.then(mergeFactory.addToIndex)
-     //    	.then(mergeFactory.postMergeCommit)
-     //    	// .then(function (repo) {
-     //    	// 	console.log('got here', repo)
-     //    	// })
-     //        .then(function (repo) {
-     //            $scope.mergeSuccess = true;
-     //        })
-     //    	.catch(function (err) {
-     //            console.log(err)
-     //            if (err.message === 'This merge has conflicts') {
-     //                console.log("we made it past the if statement")
-     //                mergeFactory.getConflicts()
-     //                    .then(function (conflictFiles) {
-     //                        $scope.conflictFiles = conflictFiles
-     //                        $scope.mergeErr = err.message
-     //                })
-     //            } else {
-     //                $scope.mergeErr = err.message
-     //            }
-        		
-     //    	})
-        	
-        	
 
-        
     }
 
     $scope.findConflicts = function () {
--- window/pull/pull_factory.js
+++ window/pull/pull_factory.js
@@ -2,18 +2,19 @@ app.factory('pullFactory', function ($rootScope, $state, commitFactory){
 
     return {
         pullRepo: function(){
-        	commitFactory.unstaged().then(function(data){
-        		console.log('pull', data);
-	        	if (data.length < 1){
-		          $rootScope.repo.remote_fetch("", function(err){
-		              if (err) throw err;
-		              console.log("fetched repo")
-		          })
-	        	} else {
-	        		$state.go('commit');
-	        	}
-        		
-        	})
+	        	commitFactory.unstaged().then(function(data){
+	        		console.log('pull', data);
+		        	if (data.length < 1){
+			          $rootScope.repo.remote_fetch("", function(err){
+			              if (err) throw err;
+			              console.log("fetched repo")
+			          })
+		        	} else {
+		        		$state.go('commit');
+		        	}
+	        		
+	        	})
+        	
         }
     }
 });
