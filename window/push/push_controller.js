42d9584528febbc4719172305a2fb33c572b8059
blakeprobinson <bprobinson@zoho.com>
2015-04-02 11:50:14 -0400

    changed push function to stateDotGo

--- window/push/push_controller.js
+++ window/push/push_controller.js
@@ -1,15 +1,17 @@
-app.controller('PushCtrl', function ($scope, $rootScope, branchFactory, $q) {
+app.controller('PushCtrl', function ($state, $scope, $rootScope, branchFactory, $q) {
 
     $scope.push = function () {
     	return $q(function (resolve, reject) {
-    		$rootScope.repo.remote_push("origin", branchFactory.currentBranch, function(err) {
+    		$rootScope.repo.remote_push("origin", branchFactory.currentBranch, function (err) {
     		    if (err) {reject(err)}
     		   	else {
     		   		var done = 'done'
     		   		resolve(done)
     		   	}
     		})
-    	})
+    	}).then(function (data) {
+            $state.go('end')
+        })
         
     }
 });
