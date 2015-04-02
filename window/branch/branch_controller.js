84c18677d5a48237bd120c579b39dbcabfd34399
Lynda <lynda.montgomery@me.com>
2015-04-02 13:55:15 -0400

    add delete function

--- window/branch/branch_controller.js
+++ window/branch/branch_controller.js
@@ -1,6 +1,17 @@
 app.controller('BranchCtrl', function ($scope, $state, $rootScope, branches, branchFactory, pullFactory) {
-
+  var deleteBranches = [];
   $scope.branches = branches;
+
+  
+  $scope.deleteBranches = function(){
+    for (var i=0; i<branches.length; i++){
+      if (!(value==='master')) deleteBranches.push(value);
+    }
+    return deleteBranches;
+  }
+
+  
+
 
   $scope.switch = function (branchName) {
     console.log('switch', branchName);
@@ -24,8 +35,20 @@ app.controller('BranchCtrl', function ($scope, $state, $rootScope, branches, bra
       }).then(function (data) {
         $state.go('work');
       })
-  	
   }
 
+  $scope.deleteBranch = function(branchName) {
+    console.log('deleteBranch', branchName);
+    branchFactory.switchBranch('master').then(function(data){
+      console.log('after switch');
+      return branchFactory.deleteOldBranch(branchName)
+    })
+    .then(function(data){
+      console.log('after delete');
+      return branchFactory.getAllBranches().then(function(branches){
+        $scope.branches = branches;
+      })
+    })
+  }
 
 });
